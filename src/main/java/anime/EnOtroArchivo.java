4edcb3125f1778b8488fc84b7c4cdd3b784907c7
Rommel05 <rrll20220@gmail.com>
2024-05-06 10:27:56 +0200

    Api

--- src/main/java/anime/EnOtroArchivo.java
+++ src/main/java/anime/EnOtroArchivo.java
@@ -2,23 +2,22 @@ package anime;
 
 import com.google.gson.Gson;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-
-public class Main {
+public class EnOtroArchivo {
     public static void main(String[] args) throws IOException {
         final Gson gson = new Gson();
         URL f = new URL("https://www.arbeitnow.com/api/job-board-api");
         BufferedReader in = new BufferedReader(new InputStreamReader(f.openStream(), StandardCharsets.UTF_8));
         Job j = gson.fromJson(in, Job.class);
 
-        for (Job.data job : j.data ) {
-            System.out.println(job);
-        }
+        FileWriter fw = new FileWriter("/home/INFORMATICA/alu10637835/IdeaProjects/ejercicios/json/src/main/java/anime/a1.txt");
 
+        for (Job.data job : j.data ) {
+            fw.write(job.toString());
+            fw.write("\n");
+        }
     }
 }
